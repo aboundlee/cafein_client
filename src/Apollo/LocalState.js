cfc84801cf976e7a7d9c39abb5c2d16fd863c75d
EC2 Default User <ec2-user@ip-172-31-47-153.ap-northeast-2.compute.internal>
2020-08-27 10:17:54 +0000

    cafe 50 data

--- src/Apollo/LocalState.js
+++ src/Apollo/LocalState.js
@@ -1,5 +1,4 @@
 export const defaults = {
-    isLoggedIn: Boolean(localStorage.getItem("token")) || false,
     markerStatus: localStorage.getItem("markerStatus") !== undefined ? localStorage.getItem("markerStatus") : "text"
     
 };
@@ -12,20 +11,6 @@ export const resolvers = {
         }
     },
     Mutation: {
-        logUserIn: (_, {token}, {cache}) => {
-            localStorage.setItem("token",token);
-            cache.writeData({
-                data: {
-                    isLoggedIn: true
-                }
-            });
-            return null;
-        },
-        logUserOut: (_, __, {cache}) => {
-            localStorage.removeItem("token");
-            window.location.reload();
-            return null;
-        },
 
         Check: (_, __, {cache}) => {
             localStorage.removeItem("token");
